5b25b699a5e115252b1019f60308cd6b4dc431d7
alysl56 <alysl56@nottingham.ac.uk>
2025-04-23 07:18:55 +0100

    Create pcg_blood_count.sh

---
+++ Project1/pcg_blood_count.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=300g
+#SBATCH --time=48:00:00
+#SBATCH --job-name=htseq-count_blood_pcg
+#SBATCH --output=/share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/logs/slurm-%x-%j.out
+#SBATCH --error=/share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/logs/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbxjy4@exmail.nottingham.ac.uk
+
+source $HOME/.bash_profile
+
+conda activate Rotation2
+
+htseq-count -f bam -r pos -s no -i ID -t protein_coding_gene /share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/mapped/igv/bloodsorted.bam /share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/ref/ref.gff >$$.gff > bloodcount.txt
+
+conda deactivate

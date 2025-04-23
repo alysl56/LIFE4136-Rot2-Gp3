01205496637e3ea8850103ec35ce39e3f66e6821
alysl56 <alysl56@nottingham.ac.uk>
2025-04-23 07:23:17 +0100

    Create htseq-count_fat.sh

---
+++ Project1/htseq-count_fat.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=300g
+#SBATCH --time=48:00:00
+#SBATCH --job-name=htseq-count_fat
+#SBATCH --output=/share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/logs/slurm-%x-%j.out
+#SBATCH --error=/share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/logs/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbxjy4@exmail.nottingham.ac.uk
+
+source $HOME/.bash_profile
+
+conda activate Rotation2
+
+htseq-count /share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/mapped/fatmapped.sam /share/BioinfMSc/Bill_resources/Tbrucei/fastq/Group3/ref/ref.gff
+
+conda deactivate
